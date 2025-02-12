2d98f26c48a7eb8e5301e6d8ec8e41137ccf7338
Levi_PM241595 <151584999+most-improve123@users.noreply.github.com>
2025-02-11 19:10:38 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import Contador from './Contador';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Contador />
+    </div>
+  );
+}
+
+export default App;
